8d4e6fc593f8b855d02b531300ea8d8cd554b97f
Bidyut Debnath <1992bidyut@gmail.com>
2020-11-10 22:47:50 +0600

    class 5

---
+++ class 5/class_5/class_5.ino
@@ -0,0 +1,37 @@
+#include <SoftwareSerial.h>
+#define rxPin 8
+#define txPin 9
+SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+
+int val=0;
+double voltage=0.0;
+int angVal=0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(5,INPUT);
+  pinMode(6,OUTPUT);
+  pinMode(11,OUTPUT);
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  Serial.println("UNO Setup ok");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(5)==LOW){
+    digitalWrite(6,HIGH);
+    mySerial.println("Switch ON from UNO");
+  }else{
+    digitalWrite(6,LOW);
+    mySerial.println("Switch OFF from UNO");
+  }
+  val=analogRead(A0);
+  voltage=(val*0.0048828125)*10;
+  Serial.println(voltage);
+  analogWrite(11,angVal);
+  delay(1000);
+  if(angVal>=255){
+    angVal=0;
+  }
+  angVal=angVal+20;
+}
---
+++ class 5/readDataFromUNO/readDataFromUNO.ino
@@ -0,0 +1,28 @@
+// include the SoftwareSerial library so you can use its functions:
+#include <SoftwareSerial.h>
+
+#define rxPin 10
+#define txPin 11
+char cRead;
+
+// set up a new serial port
+SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+
+void setup()  {
+  // define pin modes for tx, rx:
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  mySerial.println("Setup ok MINI");
+}
+
+void loop() {
+  if (Serial.available()>0){
+    cRead=Serial.read();
+  mySerial.print(cRead);
+ }else{
+//  Serial.println("Not available!");
+ }
+}
