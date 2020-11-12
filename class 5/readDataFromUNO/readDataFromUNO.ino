8c0cef343758d88b2dda23df3a4c78c7a7442fa8
Bidyut Debnath <1992bidyut@gmail.com>
2020-11-13 01:08:36 +0600

    tx rx pin connection error fix

--- class 5/readDataFromUNO/readDataFromUNO.ino
+++ class 5/readDataFromUNO/readDataFromUNO.ino
@@ -15,7 +15,7 @@ void setup()  {
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
   Serial.begin(9600);
-  mySerial.println("Setup ok MINI");
+  mySerial.println("NANO Setup ok");
 }
 
 void loop() {
